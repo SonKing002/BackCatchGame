5bf35f0ab0ca10e68209227e875f08499525fddc
SonKing002 <101817311+SonKing002@users.noreply.github.com>
2024-04-11 19:00:44 +0900

    타이틀 디자인

---
+++ Assets/03_Scripts/InGame/Controller/InTitleSenser.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InTitleSensor : SingletonOfT<InTitleSensor>
+{
+    [SerializeField]
+    private Transform[] transform;
+
+
+    private void Awake()
+    {
+        base.Awake();
+
+        DontDestroyOnLoad(this);
+    }
+
+    void Update()
+    {
+        
+    }
+}
