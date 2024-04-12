cc81aa3f65dcac4db0ac951a49b84826f48aecd3
chogo96 <miin9517@gmail.com>
2024-04-12 09:16:05 +0900

    PlayerController
    점프 추가 중간저장

--- Assets/03_Scripts/InGame/PlayerController.cs
+++ Assets/03_Scripts/InGame/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,39 +13,36 @@ public class PlayerController : MonoBehaviour
     //인스펙터에서 등록할 inputActionAsset
     public InputActionAsset inputActionAsset;
     //입력값을 저장
-    private Vector2 _inputValue;
-    //점프입력 값을 저장
-    private float _inputJumpValue;
-    //RigidBody
-    private Rigidbody _rigidBody;
-    private float _gravity;
+    private Vector2 _inputMoveValue;
+    //중력
+    [SerializeField]private float _gravity = -9.8f;
     //플레이어 앞뒤 움직임 속도
     [SerializeField] private float _moveSpeed = 4.0f;
     //플레이어 회전 속도
     [SerializeField] private float _rotateSpeed = 4.0f;
+    //플레이어 점프력
     [SerializeField] private float _jumpPower = 2.0f;
+    //지면에 닿는 거리
     [SerializeField] private float _groundDistance;
     private bool isGrounded;
     [SerializeField]private LayerMask groundLayer;
-    Transform _transform;
+    //캐릭터 컨트롤러
+    private CharacterController _characterController;
+    //수평 중력
+    private float verticalVelocity;
+
+    Vector3 moveDirection;
+
     private void Awake()
     {   
-        _transform = this.transform;
-        _rigidBody = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
     }
 
-    //이동 시에 사용할 업데이트 함수 물리 연산을 포함하는 함수라 FixedUpdate에 추가했음
-    private void FixedUpdate()
+    private void Update()
     {
-        Move(_inputValue.y);
-        Rotate(_inputValue.x);
+        Move();
+        Rotate(_inputMoveValue.x);
         Debug.DrawRay(transform.position, Vector3.down, Color.red, _groundDistance);
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, _groundDistance, groundLayer);
-        if (isGrounded)
-        {
-            Jump();
-        }
-        
     }
 
     //좌우의 회전을 관리하는 함수.
@@ -53,24 +51,41 @@ public class PlayerController : MonoBehaviour
         transform.localRotation *= Quaternion.Euler(0f, input * _rotateSpeed * Time.deltaTime, 0f);
     }
     //앞 뒤의 움직임을 관리하는 함수
-    private void Move(float input)
+    private void Move()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, _groundDistance, groundLayer);
+        Debug.Log("isGrounded: " + isGrounded);
 
-        //TODO rigidbody 빼고만들기
-        Vector3 newPosition = _rigidBody.position + (transform.forward * input * _moveSpeed * Time.deltaTime);
+        Vector3 move = new Vector3(_inputMoveValue.x, 0, _inputMoveValue.y) * _moveSpeed;
+
+        if (isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = 0;  
+        }
+        else
+        {
+            verticalVelocity -= _gravity * Time.deltaTime;
+        }
+        move.y = verticalVelocity;
+        _characterController.Move(move * Time.deltaTime);
     }
     private void Jump()
     {
-        //TODO rigidbody 빼고 만들기
+        if (isGrounded)
+        {
+            verticalVelocity = _jumpPower;
+        }
     }
-    //인풋 액션으로 OnMove값의 입력값을 받는다.
     public void OnMove(InputAction.CallbackContext context)
     {
-        _inputValue = context.ReadValue<Vector2>();
+        _inputMoveValue = context.ReadValue<Vector2>();
     }
     public void OnJump(InputAction.CallbackContext contexts)
     {
-        Debug.Log("점프 눌렀어요~");
-        _inputJumpValue = contexts.ReadValue<float>();
+        //정확하게 눌렀을 때
+        if (contexts.performed)
+        {
+            Jump();
+        }
     }
 }
